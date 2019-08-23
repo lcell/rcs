4dcb3f2c8d39218e52cd7ca9a36c731a80f07e48
jerry <geyubo90@gmail.com>
2019-08-23 16:18:37 +0800

    add dockerfile-maven-plugin

--- rcs-ui/config/dep.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  ENV_CONFIG: '"dep"',
-  BASE_API: '"http://119.3.13.175:18081/admin"'
-}
--- rcs-ui/src/views/layout/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AppHeader } from './AppHeader'
-export { default as Sidebar } from './Sidebar/index.vue'
-export { default as TagsView } from './TagsView'
-export { default as AppMain } from './AppMain'
